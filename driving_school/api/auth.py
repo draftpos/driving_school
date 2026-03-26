656c25de012b79e138f4ea7f37dce23301594e58
munyaradzi <chirovemunyaradzi@gmail.com>
2026-03-26 13:22:51 +0200

    redirects

--- driving_school/api/auth.py
+++ driving_school/api/auth.py
@@ -4,17 +4,40 @@
 import frappe
 
 
+# def on_login(login_manager):
+# 	"""Redirect Driving School Students to test page after login"""
+# 	user = login_manager.user
+
+# 	if user == "Administrator" or user == "Guest":
+# 		return
+
+# 	# Check if user has Driving School Student role
+# 	user_roles = frappe.get_roles(user)
+
+# 	if "Driving School Student" in user_roles:
+# 		# Set redirect to test page
+# 		frappe.local.response["home_page"] = "/student-test"
+# 		frappe.cache.hset("home_page", user, "/student-test")
+
+
+
 def on_login(login_manager):
-	"""Redirect Driving School Students to test page after login"""
-	user = login_manager.user
+    """Redirect Driving School Students to test page after login"""
+    user = login_manager.user
 
-	if user == "Administrator" or user == "Guest":
-		return
+    if user in ("Administrator", "Guest"):
+        return
 
-	# Check if user has Driving School Student role
-	user_roles = frappe.get_roles(user)
+    user_roles = frappe.get_roles(user)
+    if "Driving School Student" in user_roles:
+        frappe.local.response["type"] = "redirect"
+        frappe.local.response["location"] = "/student-test"
 
-	if "Driving School Student" in user_roles:
-		# Set redirect to test page
-		frappe.local.response["home_page"] = "/student-test"
-		frappe.cache.hset("home_page", user, "/student-test")
+@frappe.whitelist(allow_guest=True)
+def logout():
+    """Custom logout endpoint"""
+    frappe.local.login_manager.logout()
+
+    frappe.local.response["message"] = "Logged out"
+    frappe.local.response["type"] = "redirect"
+    frappe.local.response["location"] = "/login"   # change if needed
